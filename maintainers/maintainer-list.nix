72ab404020eea27f766af03fd3a4d30fdcdf12c6
Ankit Pandey <anpandey@protonmail.com>
2022-09-04 17:08:06 -0700

    maintainers: add anpandey

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1141,6 +1141,11 @@
     keys = [{
       fingerprint = "06E8 4FF6 0CCF 7AFD 5101  76C9 0FBC D3EE 6310 7407";
     }];
+  anpandey = {
+    email = "anpandey@protonmail.com";
+    github = "anpandey";
+    githubId = 25336974;
+    name = "Ankit Pandey";
   };
   anpryl = {
     email = "anpryl@gmail.com";
