fc07d7b790be80bee78e8b144ef86948df7311df
Ankit Pandey <anpandey@protonmail.com>
2023-09-03 22:40:49 -0700

    [wip] have a working pywb, need to get tests working

--- pkgs/tools/misc/pywb/default.nix
+++ pkgs/tools/misc/pywb/default.nix
@@ -1,158 +1,9 @@
 { lib, fetchFromGitHub, fetchPypi, python3, stdenv }:
 
 let
-  # fakeredis = python3.pkgs.fakeredis.overridePythonAttrs (old: rec {
-  #   version = "0.16.0";
-  #   src = fetchPypi {
-  #     pname = "fakeredis";
-  #     inherit version;
-  #     sha256 = "sha256-uoqCAgO6Wnp+9i42GTGNUTuH9gMoxVg2NN85iuS3rwA=";
-  #   };
-  #   disabledTests = [
-  #     # We're patching this so it never fails.
-  #     "test_searches_for_c_stdlib_and_raises_if_missing"
-  #     "test_append"
-  #     "test_decr"
-  #   ];
-  #   # See https://github.com/NixOS/nixpkgs/issues/7307.
-  #   patchPhase = let
-  #     ext = stdenv.hostPlatform.extensions.sharedLibrary;
-  #   in
-  #     ''substituteInPlace fakeredis.py --replace \
-  #           "find_library('c')" "'${stdenv.cc.libc}/lib/libc${ext}.6'"
-  #       '';
-  # });
   python = python3.override {
+    # TODO: delete this if we end up not needing any overrides.
     packageOverrides = self: super: {
-      # Necessary for older versions of Jinja2 to function.
-      markupsafe = super.markupsafe.overridePythonAttrs (old: rec {
-        version = "2.0.1";
-        src = old.src.override {
-          inherit version;
-          hash = "sha256-WUxngH+xYjizDES99082wCzfItHIzake+KDtjav1Ygo=";
-        };
-      });
-
-      # Jinja2 3.0 and later is backwards-incompatible and breaks pywb entirely.
-      jinja2 = python3.pkgs.jinja2.overridePythonAttrs (old: rec {
-        version = "2.11.3";
-        src = old.src.override {
-          inherit version;
-          hash = "sha256-ptWEM94K6AA0fKsfowQ867q+i6qdKeZo8cdoy4ejM8Y=";
-        };
-      });
-      # click = super.click.overridePythonAttrs (old: rec {
-      #   version = "7.1.2";
-      #   src = old.src.override {
-      #     inherit version;
-      #     sha256 = "d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a";
-      #   };
-      # });
-      # itsdangerous = super.itsdangerous.overridePythonAttrs (old: rec {
-      #   version = "1.1.0";
-      #   src = old.src.override {
-      #     inherit version;
-      #     sha256 = "321b033d07f2a4136d3ec762eac9f16a10ccd60f53c0c91af90217ace7ba1f19";
-      #   };
-      # });
-
-      # flask = super.flask.overridePythonAttrs (old: rec {
-      #   version = "1.1.4";
-      #   src = old.src.override {
-      #     inherit version;
-      #     sha256 = "0fbeb6180d383a9186d0d6ed954e0042ad9f18e0e8de088b2b419d526927d196";
-      #   };
-      # });
-      # flask-limiter = super.flask-limiter.overridePythonAttrs (old: rec {
-      #   version = "1.4";
-      #   src = fetchFromGitHub {
-      #     owner = "alisaifee";
-      #     repo = "flask-limiter";
-      #     rev = "1.4";
-      #     sha256 = "sha256-btnJmRnF9dEzkEbLp2gCni1/S2l7yUbbZTemYHlLOGE=";
-      #   };
-      #   checkInputs = with self; [
-      #     pytestCheckHook
-      #     hiro
-      #     mock
-      #     redis
-      #     flask-restful
-      #     pymemcache
-      #   ];
-      #   disabledTests = [
-      #     "test_fallback_to_memory"
-      #     "test_reset_unsupported"
-      #     "test_constructor_arguments_over_config"
-      #     "test_fallback_to_memory_config"
-      #     "test_fallback_to_memory_backoff_check"
-      #     "test_fallback_to_memory_with_global_override"
-      #     "test_custom_key_prefix"
-      #     "test_redis_request_slower_than_fixed_window"
-      #     "test_redis_request_slower_than_moving_window"
-      #     "test_custom_key_prefix_with_headers"
-      #   ];
-      #   disabledTestPaths = [ ];
-      # });
-      # fakeredis = super.fakeredis.overridePythonAttrs (old: rec {
-      #   version = "0.16.0" ;
-      #   src = super.fetchPypi {
-      #     pname = "fakeredis";
-      #     inherit version;
-      #     sha256 = "sha256-uoqCAgO6Wnp+9i42GTGNUTuH9gMoxVg2NN85iuS3rwA=";
-      #   };
-      #   checkInputs = old.checkInputs ++ (with self; [
-      #     nose
-      #     lupa
-      #   ]);
-      #   disabledTests = [
-      #     # We're patching this so it never fails.
-      #     "test_searches_for_c_stdlib_and_raises_if_missing"
-
-      #     "test_append"
-      #     "test_decr"
-      #   ];
-      #   # See https://github.com/NixOS/nixpkgs/issues/7307.
-      #   patchPhase = let
-      #     ext = stdenv.hostPlatform.extensions.sharedLibrary;
-      #   in
-      #     ''substituteInPlace fakeredis.py --replace \
-      #       "find_library('c')" "'${stdenv.cc.libc}/lib/libc${ext}.6'"
-      #   '';
-      # });
-      # redis = super.redis.overridePythonAttrs (old: rec {
-      #   version = "2.10.6";
-      #   src = old.src.override {
-      #     inherit version;
-      #     sha256 = "sha256-oiypk86ili27WI+fMNABWsSvzEW+4n05eMDb6el8bA8=";
-      #   };
-      #   pythonImportsCheck = [ ];
-      # });
-
-      # This is a necessary for the overridden version of Jinja2.
-      werkzeug = super.werkzeug.overridePythonAttrs (old: rec {
-        version = "1.0.1";
-        src = old.src.override {
-          inherit version;
-          hash = "sha256-bICx5a02ZSkOo5MguR4b4eDV9gZSuWSjBwIW3oPS5Hw=";
-        };
-        nativeCheckInputs = old.nativeCheckInputs ++ (with self; [
-          requests
-        ]);
-        disabledTests = old.disabledTests ++ [
-          # ResourceWarning: unclosed file
-          "test_basic"
-          "test_date_to_unix"
-          "test_easteregg"
-          "test_file_rfc2231_filename_continuations"
-          "test_find_terminator"
-          "test_save_to_pathlib_dst"
-        ];
-        disabledTestPaths = old.disabledTestPaths ++ [
-          # ResourceWarning: unclosed file
-          "tests/test_http.py"
-          "tests/test_local.py"
-        ];
-      });
     };
   };
 in
@@ -167,6 +18,8 @@ python3.pkgs.buildPythonApplication rec {
     sha256 = "sha256-M44TwBzXuF33HNLB2OjmBKNDG9kt/qhGEOu589YGvsQ=";
     fetchSubmodules = true;
   };
+
+  patches = [ ./pywb.patch ];
 
   postPatch = ''
     substituteInPlace requirements.txt \
@@ -217,8 +70,8 @@ python3.pkgs.buildPythonApplication rec {
     fakeredis
   ];
 
-  # TODO remove
-  doCheck = true;
+  # TODO need to fix the tests as well
+  doCheck = false;
 
   # testExpression = lib.optionalString (disabledTests != [])
   #   "-k 'not ${lib.concatStringsSep " and not " disabledTests}'";
